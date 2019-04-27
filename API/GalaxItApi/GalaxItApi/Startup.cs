3a419bcd46f3218a4dbd63eada493e7d1d2fcf49
Tinaël <tinael.devresse@mic-belgique.be>
2019-04-27 11:32:40 +0200

    Add Swashbuckle for SwaggerGen to generate client code

--- API/GalaxItApi/GalaxItApi/Startup.cs
+++ API/GalaxItApi/GalaxItApi/Startup.cs
@@ -5,6 +5,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using GalaxItApi.Data;
 using Microsoft.EntityFrameworkCore;
+using Swashbuckle.AspNetCore.Swagger;
 
 namespace GalaxItApi
 {
@@ -23,6 +24,7 @@ namespace GalaxItApi
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
             services.AddDbContext<GalaxitContext>
                 (options => options.UseSqlServer(Configuration.GetConnectionString("GalaxitContext")));
+            services.AddSwaggerGen(c => { c.SwaggerDoc("v1", new Info {Title = "My API v1", Version = "v1"}); });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -37,6 +39,9 @@ namespace GalaxItApi
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API v1"));
 
             app.UseHttpsRedirection();
             app.UseMvc();
