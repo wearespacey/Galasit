c523e77f16ce5bf0d81d3f552d90b7be503b30b0
Tinaël <tinael.devresse@mic-belgique.be>
2019-04-27 14:55:03 +0200

    Add routes

--- Angular/GalaxIt/src/app/modules/app-routing.module.ts
+++ Angular/GalaxIt/src/app/modules/app-routing.module.ts
@@ -1,9 +1,32 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { BackgroundComponent } from '../components/background/background.component';
 import { LoginComponent } from '../components/login/login.component';
+import { RegisterComponent } from '../components/register/register.component';
+import { BubbleComponent } from '../components/bubble/bubble.component';
+import { UnbookComponent } from '../components/unbook/unbook.component';
+import { SelectionComponent } from '../components/selection/selection.component';
+import { SeatInfoComponent } from '../components/seat-info/seat-info.component';
 import { PageNotFoundComponent } from '../components/page-not-found/page-not-found.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '', component: BackgroundComponent, 
+    children: [
+      { path: 'login', component: LoginComponent },
+      { path: 'register', component: RegisterComponent },
+      { path: 'bubble', component: BubbleComponent },
+      { path: 'unbook', component: UnbookComponent }
+    ]
+  },
+  {
+    path: '', component: SelectionComponent, 
+    children: [
+      { path: 'seat/:id', component: SeatInfoComponent }
+    ]
+  },
+  { path: '**', component: PageNotFoundComponent }
+]
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
